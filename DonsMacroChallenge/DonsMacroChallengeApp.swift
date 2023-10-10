b7d0ea63863ea2b633ffe440e7d1a401358a0000
Don <neveradio@gmail.com>
2023-10-10 23:53:54 +0900

    Initial Commit

---
+++ DonsMacroChallenge/DonsMacroChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DonsMacroChallengeApp.swift
+//  DonsMacroChallenge
+//
+//  Created by Kimdohyun on 2023/10/10.
+//
+
+import SwiftUI
+
+@main
+struct DonsMacroChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
