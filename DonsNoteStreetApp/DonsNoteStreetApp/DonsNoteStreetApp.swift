af942daa2277d383a549a4b25ef7f16a0a7031be
Don <neveradio@gmail.com>
2023-11-12 19:30:21 +0900

    [feat]try

--- DonsNoteStreetApp/DonsNoteStreetApp/DonsNoteStreetApp.swift
+++ DonsNoteStreetApp/DonsNoteStreetApp/DonsNoteStreetApp.swift
@@ -10,17 +10,9 @@ import GoogleMaps
 import GooglePlaces
 import AuthenticationServices
 
-class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        return true
-    }
-}
-
 @main
 struct DonsNoteStreetAppApp: App {
 //MARK: - 1. PROPERTY
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @StateObject var service = Service()
     
     let APIKey = "AIzaSyA40AwM5IZVWhlkPqVKCipY7GHQfNqX_LI"
